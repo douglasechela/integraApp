d4a8e967701edad9ec85babf5f0034f25f493161
Douglas Echela <douglas.casagrande01@etec.sp.gov.br>
2020-11-25 22:07:43 -0300

    integraApp
    desenvolvendo novo projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'categorias/listar',
+    loadChildren: () => import('./categorias/listar/listar.module').then( m => m.ListarPageModule)
+  },
+  {
+    path: 'categorias/cadastrar',
+    loadChildren: () => import('./categorias/cadastrar/cadastrar.module').then( m => m.CadastrarPageModule)
+  },
+  {
+    path: 'categorias/alterar/:id',
+    loadChildren: () => import('./categorias/alterar/alterar.module').then( m => m.AlterarPageModule)
   },
 ];
 
@@ -19,4 +31,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
